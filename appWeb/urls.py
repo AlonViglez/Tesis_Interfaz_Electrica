ef9503ddee1c34df3f443077a6e17bd0f8e28662
franciscofs2003 <99775859+franciscofs2003@users.noreply.github.com>
2024-03-21 07:28:13 -0600

    barra lateral finish

--- appWeb/urls.py
+++ appWeb/urls.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
-from login import views 
+from login import views as login_views
+from dashboard import views as dashboard_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home'),
-    path('registrar/', views.registrar, name='registrar')
+    path('', login_views.home, name='home'),
+    path('registrar/', login_views.registrar, name='registrar'),   
+    path('dashboard/', dashboard_views.dashboard, name='dashboard'),  
 ]
+
+
+
