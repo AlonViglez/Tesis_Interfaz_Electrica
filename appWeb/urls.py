6fefa11133714030e9d5a40692b4088ebf191ecf
AlonViglez <jesusalonsovilla2002@outlook.com>
2024-04-02 14:11:19 -0600

    Login/Registro
    Implemente la base de datos sqlite(solo una tabla para el alumno).
    Agregue una funcion para que la cuenta este activa.
    URLS protegidas.
    Funcionalidad del login y registro.

--- appWeb/urls.py
+++ appWeb/urls.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
 from django.urls import path, include
-from login import views 
+from django.contrib.auth.decorators import login_required #Para autenticacion
+from login import views as login_views
+#from login import views as registrar_views
+#from login import views as autenticacion_views
+#from login import views as logueado_views
+#from login import views as registrar_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home'),
-    path('registrar/', views.registrar, name='registrar')
+    path('', login_views.home, name='home'),
+    path('registrar/', login_views.registrar, name='registrar'),
+    path('autenticacion/', login_views.autenticacion, name='autenticacion'),
+    path('logout/', login_views.logout_view, name='logout'),
+    path('logueado/', login_views.logueado, name='logueado'),
 ]
--- login/models.py
+++ login/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.core.validators import RegexValidator
 
-# Create your models here.
+class Usuario(models.Model):
+    #Campos de texto
+    nombre = models.CharField(max_length=50)
+    email = models.EmailField(max_length=50)
+    materia = models.CharField(max_length=30)
+
+    #Campos numéricos
+    numero_cuenta = models.CharField(max_length=9, validators=[RegexValidator(r'^\d{1,10}$')])
+    #Campos contraseñas
+    password = models.CharField(max_length=50)
+
+    def __str__(self):
+        return str(self.numero_cuenta)
+
