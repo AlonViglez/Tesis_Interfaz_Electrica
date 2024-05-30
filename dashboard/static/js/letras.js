806ed7502e5dcdd7588d82b4a3f5c72dbe388f01
franciscofs2003 <99775859+franciscofs2003@users.noreply.github.com>
2024-05-30 07:30:51 -0600

    accesibilidad
    aumento del tamaño de letra y el fondo oscuro y blanco

---
+++ dashboard/static/js/letras.js
@@ -0,0 +1,10 @@
+function increaseTextSize() {
+    document.body.classList.add('large-text');
+}
+function toggleContrast() {
+    document.body.classList.toggle('dark-mode');
+}
+
+function decreaseTextSize() {
+    document.body.classList.remove('large-text');
+}
